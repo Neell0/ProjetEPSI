c8c72d514ea7e0ef75207a5dfd53f5674f3793a9
Ethan DIVET <ethan.divet@bimeo.fr>
2025-02-05 14:24:43 +0100

    claculet end

--- script.js
+++ script.js
@@ -2,42 +2,41 @@ let display = document.getElementById('display');
 let currentOperation = '';
 let firstOperand = '';
 let secondOperand = '';
+let shouldResetDisplay = false;
 
 function appendNumber(number) {
+    if (shouldResetDisplay) {
+        display.value = '';
+        shouldResetDisplay = false;
+    }
     display.value += number;
 }
 
 function setOperation(operation) {
     if (display.value === '') return;
+    if (currentOperation !== '') {
+        calculate();
+    }
     firstOperand = display.value;
     currentOperation = operation;
-    display.value = '';
+    display.value += operation;
+    shouldResetDisplay = false;
 }
 
 function calculate() {
-    if (display.value === '' || firstOperand === '') return;
-    secondOperand = display.value;
-    let result;
-    switch (currentOperation) {
-        case '+':
-            result = parseFloat(firstOperand) + parseFloat(secondOperand);
-            break;
-        case '-':
-            result = parseFloat(firstOperand) - parseFloat(secondOperand);
-            break;
-        case '*':
-            result = parseFloat(firstOperand) * parseFloat(secondOperand);
-            break;
-        case '/':
-            result = parseFloat(firstOperand) / parseFloat(secondOperand);
-            break;
-        default:
-            return;
+    let expression = display.value;
+    try {
+        let result = eval(expression);
+        display.value = result;
+        firstOperand = result;
+        currentOperation = '';
+        shouldResetDisplay = true;
+    } catch (error) {
+        display.value = 'Erreur';
+        firstOperand = '';
+        currentOperation = '';
+        shouldResetDisplay = true;
     }
-    display.value = result;
-    firstOperand = '';
-    secondOperand = '';
-    currentOperation = '';
 }
 
 function clearDisplay() {
@@ -45,4 +44,5 @@ function clearDisplay() {
     firstOperand = '';
     secondOperand = '';
     currentOperation = '';
+    shouldResetDisplay = false;
 }
